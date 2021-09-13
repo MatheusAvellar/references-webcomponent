c1843abb5500697173ea76d89c7632a772d9d72a
Matheus Avellar <MatheusAvellar@users.noreply.github.com>
2021-09-12 23:13:06 -0300

    Move to a single file

--- references-wc.js
+++ references-wc.js
@@ -1,4 +1,5 @@
-class ReferencesElement extends HTMLElement {
+// <references-list>
+class ReferencesList extends HTMLElement {
   state = {
     ref_names: {}
   };
@@ -153,4 +154,47 @@ class ReferencesElement extends HTMLElement {
   }
 }
 
-customElements.define("references-list", ReferencesElement);
+// <cite-web>
+class CiteWeb extends HTMLElement {
+  static get observedAttributes() { return ["data-refid"]; }
+
+  constructor() {
+    self = super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  attributeChangedCallback(name, old_value, new_value) {
+    while(this.shadowRoot.firstChild)
+      this.shadowRoot.firstChild.remove();
+
+    const reflist = document.querySelector("references-list");
+
+    const sup = document.createElement("sup");
+      const anchor = document.createElement("a");
+      anchor.setAttribute("href", `#ref${new_value}`);
+      // TODO: wiki-like clickable ↑ or a,b,c's in citations to go back to text
+      // anchor.setAttribute("id", `cite${new_value}`);
+      anchor.textContent = `[${new_value}]`;
+      anchor.onclick = e => {
+        const citation = reflist.shadowRoot.getElementById(`ref${new_value}`);
+        citation.scrollIntoView(true);
+        citation.focus();
+      };
+    sup.appendChild(anchor);
+    // For some reason, `self` here is <references-list>; so we use `this` instead
+    this.shadowRoot.appendChild(sup);
+
+    // Default style to prevent whitespace showing up if reflist uses e.g.
+    // <span slot="style"> <style>...</style> <link/> </span>
+    const default_css = document.createElement("style");
+    default_css.textContent = "[slot=\"style\"] { display: none; }";
+    this.shadowRoot.appendChild(default_css);
+    // If <references-list> has a style within it, add it to this as well
+    const css = reflist.shadowRoot.querySelector('[slot="style"]');
+    if(css) this.shadowRoot.appendChild(css.cloneNode(true));
+  }
+}
+
+// Define custom elements
+customElements.define("references-list", ReferencesList);
+customElements.define("cite-web", CiteWeb);
